371c12331b3298d41c8d96f7f9c7b6f7ff2969f5
Toast Kid <super_q1m@yahoo.co.jp>
2018-02-14 00:48:51 +0900

    Initial commit.

---
+++ app/src/test/java/jp/toastkid/til/ResourceClosingTest.kt
@@ -0,0 +1,66 @@
+package jp.toastkid.til
+
+import io.reactivex.Single
+import okio.Okio
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.io.IOException
+import java.io.InputStream
+
+/**
+ * @author toastkidjp
+ */
+class ResourceClosingTest {
+
+    private var inputStream: InputStream? = null
+
+    @BeforeEach
+    fun setUp() {
+        inputStream = javaClass.classLoader.getResourceAsStream("text/sample.txt")
+    }
+
+    @Test
+    fun test_try_finally() {
+        try {
+            inputStream?.let { nonNullInputStream ->
+                 println(Okio.buffer(Okio.source(nonNullInputStream)).readUtf8())
+            }
+        } finally {
+            inputStream?.close()
+        }
+
+        Assertions.assertThrows(
+                IOException::class.java,
+                { inputStream?.read(ByteArray(1), 0, 1) }
+        )
+    }
+
+    @Test
+    fun test_use() {
+        inputStream?.let { nonNullInputStream ->
+            Okio.source(nonNullInputStream).use { println(Okio.buffer(it).readUtf8()) }
+            Assertions.assertThrows(
+                    IOException::class.java,
+                    { nonNullInputStream.read(ByteArray(1), 0, 1) }
+            )
+        }
+    }
+
+    @Test
+    fun test_using() {
+        inputStream?.let { nonNullInputStream ->
+            val text = Single.using<String, InputStream>(
+                    { nonNullInputStream },
+                    { Single.just<String>(Okio.buffer(Okio.source(it)).readUtf8()) },
+                    { it.close() }
+            )
+                    .blockingGet()
+            println(text)
+            Assertions.assertThrows(
+                    IOException::class.java,
+                    { nonNullInputStream.read(ByteArray(1), 0, 1) }
+            )
+        }
+    }
+}
---
+++ app/src/test/java/jp/toastkid/til/TextUtilsTest.java
@@ -0,0 +1,103 @@
+package jp.toastkid.til;
+
+import android.text.TextUtils;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(RobolectricTestRunner.class)
+public class TextUtilsTest {
+
+    @Test
+    public void check_concat() {
+        assertEquals("abc", TextUtils.concat("a", "b", "c"));
+        assertEquals(0, TextUtils.concat().length());
+    }
+
+    @Test
+    public void check_expandTemplate() {
+        assertEquals("Ice cream is cool.", TextUtils.expandTemplate("^1 is ^2.", "Ice cream", "cool").toString());
+        assertEquals("cool is Ice cream.", TextUtils.expandTemplate("^2 is ^1.", "Ice cream", "cool").toString());
+        assertEquals("Ice cream is Ice cream.", TextUtils.expandTemplate("^1 is ^1.", "Ice cream", "cool").toString());
+        assertEquals("Ice cream is cool.", TextUtils.expandTemplate("Ice cream is cool.").toString());
+    }
+
+    @Test
+    public void check_isDigitsOnly() {
+        assertTrue(TextUtils.isDigitsOnly("123456"));
+        assertFalse(TextUtils.isDigitsOnly("123g"));
+        assertTrue(TextUtils.isDigitsOnly(""));
+        // TextUtils.isDigitsOnly(null);
+    }
+
+    @Test
+    public void check_htmlEncode() {
+        assertEquals("&lt;a&gt;", TextUtils.htmlEncode("<a>"));
+        assertEquals("", TextUtils.htmlEncode(""));
+        //assertEquals(null, TextUtils.htmlEncode(null));
+    }
+
+    @Test
+    public void check_join() {
+        assertEquals("abcde", TextUtils.join("", new String[]{"a", "b", "c", "d", "e"}));
+        assertEquals("a,b,c,d,e", TextUtils.join(",", new String[]{"a", "b", "c", "d", "e"}));
+        assertEquals("anullbnullcnulldnulle", TextUtils.join(null, new String[]{"a", "b", "c", "d", "e"}));
+    }
+
+    @Test
+    public void check_split() {
+        assertEquals("[to, be, or, not, to]", Arrays.toString(TextUtils.split("to/be/or/not/to", "/")));
+    }
+
+    @Test
+    public void check_substring() {
+        assertEquals("om", TextUtils.substring("tomato", 1, 3));
+        // TextUtils.substring(null, 1, 3);
+        // TextUtils.substring("tomato", -1, 3);
+        // TextUtils.substring("tomato", 2, 30);
+    }
+
+    @Test
+    public void check_replace() {
+        assertEquals("cbi", TextUtils.replace("abc", new String[]{"a", "c"}, new CharSequence[]{"c", "i"}).toString());
+        assertEquals("cbida", TextUtils.replace("abcda", new String[]{"a", "c"}, new CharSequence[]{"c", "i"}).toString());
+        assertEquals("cbida", TextUtils.replace("abcda", new String[]{"c", "a"}, new CharSequence[]{"i", "c"}).toString());
+        assertEquals("cbida", TextUtils.replace("abcda", new String[]{"c", "a"}, new CharSequence[]{"i", "c", "e"}).toString());
+        assertEquals("cbida", TextUtils.replace("abcda", new String[]{"c", "a", "e"}, new CharSequence[]{"i", "c"}).toString());
+    }
+
+    @Test
+    public void check_getTrimmedLength() {
+        assertEquals(1, TextUtils.getTrimmedLength("a  "));
+        assertEquals(4, TextUtils.getTrimmedLength("a  b"));
+        assertEquals(4, TextUtils.getTrimmedLength("  a  b  "));
+        assertEquals(0, TextUtils.getTrimmedLength(""));
+        //assertEquals(0, TextUtils.getTrimmedLength(null));
+    }
+
+    @Test
+    public void check_SimpleStringSplitter() {
+        TextUtils.SimpleStringSplitter splitter = new TextUtils.SimpleStringSplitter('-');
+        splitter.setString("a-b-c");
+        for (;splitter.hasNext();) {
+            System.out.println(splitter.next());
+        }
+        splitter.setString("9");
+        for (;splitter.hasNext();) {
+            System.out.println(splitter.next());
+        }
+        // splitter.setString(null);
+    }
+}
